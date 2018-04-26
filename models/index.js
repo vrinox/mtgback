3254fa821a6b6238e7c5a72f4fece55eedf69e37
vrinox <xonirv@gmail.com>
2018-04-26 12:56:48 +0200

    conexion que heroku config

--- models/index.js
+++ models/index.js
@@ -9,13 +9,7 @@ var db        = {};
 var sequelize;
 if (CONFIG.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL);
   } else {
     // the application is executed on the local machine ... use mysql
     sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, CONFIG.db_password, {
