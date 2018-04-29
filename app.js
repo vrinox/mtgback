3503f705d377548db2458201fcaa05f23cae170e
vrinox <xonirv@gmail.com>
2018-04-29 14:24:24 +0200

    ejs

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const custom = function(req, res, next){
   }
   next();
 }
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
