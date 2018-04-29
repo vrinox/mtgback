b6b9f0dfd6fafd6773b69dc981ef88e77c9ff552
vrinox <xonirv@gmail.com>
2018-04-29 23:18:38 +0200

    mazos ruta

--- routes/mazo.js
+++ routes/mazo.js
@@ -2,7 +2,7 @@ const Controller = require('./../controllers/mazo');
 const passport      	  = require('passport');
 
 const rutas = function(router){
-  router.get(   '/mazo',     passport.authenticate('jwt', {session:false}), Controller.getAll);
+  router.get(   '/mazos',     passport.authenticate('jwt', {session:false}), Controller.getAll);
   router.get(   '/mazo/:id', passport.authenticate('jwt', {session:false}), Controller.get);
   router.post(  '/mazo',     passport.authenticate('jwt', {session:false}), Controller.create);
   router.put(   '/mazo/:id', passport.authenticate('jwt', {session:false}), Controller.update);
