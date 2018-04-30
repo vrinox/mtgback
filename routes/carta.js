c48db2b890e1b45fe3be110035b5d4cc8cfbacac
vrinox <xonirv@gmail.com>
2018-04-30 13:23:54 +0200

    no entiendo

--- routes/carta.js
+++ routes/carta.js
@@ -2,8 +2,8 @@ const Controller = require('./../controllers/carta');
 
 const rutas = function(router){
   router.post(  '/cartas'         ,  Controller.getAll);
-  router.get(   '/carta/:id'      ,  Controller.get);
   router.get(   '/cartas/:nombre' ,  Controller.getPorNombre);
+  router.get(   '/carta/:id'      ,  Controller.get);
 }
 
 module.exports = rutas;
