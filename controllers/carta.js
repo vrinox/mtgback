1acf882a0a9f836a59a8c8d5cdca20d333994e94
vrinox <xonirv@gmail.com>
2018-04-30 13:30:47 +0200

    no es necesario

--- controllers/carta.js
+++ controllers/carta.js
@@ -17,17 +17,6 @@ const getAll = async function(req, res){
 
 module.exports.getAll = getAll;
 
-const getPorNombre = async function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    let err, cartas, filtros;
-    filtros = {"name":"'"+req.params.nombre+"'"};
-
-    [err, cartas] = await to(mtg.card.where(filtros));
-    if(err) ReE(res, err, 422);
-    return ReS(res, {"cartaa":carta});
-}
-module.exports.getPorNombre = getPorNombre;
-
 const get = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
     let idCarta,carta;
--- routes/carta.js
+++ routes/carta.js
@@ -2,7 +2,6 @@ const Controller = require('./../controllers/carta');
 
 const rutas = function(router){
   router.post(  '/cartas'         ,  Controller.getAll);
-  router.get(   '/cartas/:nombre' ,  Controller.getPorNombre);
   router.get(   '/carta/:id'      ,  Controller.get);
 }
 
