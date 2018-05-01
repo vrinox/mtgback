fe3731f6f328a1c9b5952f078e96940d41d383e9
vrinox <xonirv@gmail.com>
2018-05-01 11:10:29 +0200

    semantica de la ruta mazo

--- routes/mazo.js
+++ routes/mazo.js
@@ -2,12 +2,12 @@ const Controller = require('./../controllers/mazo');
 const passport   = require('passport');
 
 const rutas = function(router){
-  router.get(   '/mazos'        , passport.authenticate('jwt', {session:false}), Controller.getAll);
-  router.get(   '/mazo/:id'     , passport.authenticate('jwt', {session:false}), Controller.get);
-  router.post(  '/mazo'         , passport.authenticate('jwt', {session:false}), Controller.create);
-  router.post(  '/mazo/agregar' , passport.authenticate('jwt', {session:false}), Controller.agregarCarta);
-  router.put(   '/mazo/:id'     , passport.authenticate('jwt', {session:false}), Controller.update);
-  router.delete('/mazo/:id'     , passport.authenticate('jwt', {session:false}), Controller.remove);
+  router.get(   '/mazos'            , passport.authenticate('jwt', {session:false}), Controller.getAll);
+  router.get(   '/mazo/:id'         , passport.authenticate('jwt', {session:false}), Controller.get);
+  router.post(  '/mazo'             , passport.authenticate('jwt', {session:false}), Controller.create);
+  router.post(  '/mazo/agregarCarta', passport.authenticate('jwt', {session:false}), Controller.agregarCarta);
+  router.put(   '/mazo/:id'         , passport.authenticate('jwt', {session:false}), Controller.update);
+  router.delete('/mazo/:id'         , passport.authenticate('jwt', {session:false}), Controller.remove);
 }
 
 module.exports = rutas;
