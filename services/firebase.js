810840423046b724bd0abf9d526fd5d7c88440f9
vrinox <xonirv@gmail.com>
2018-05-20 21:35:07 +0200

    config

--- services/firebase.js
+++ services/firebase.js
@@ -1,7 +1,7 @@
 const firebase_init = function(){
   //firebase
   var firebase = require("firebase-admin");
-  var serviceAccount = require("./config/firebase_secret.json");
+  var serviceAccount = require("../config/firebase_secret.json");
   firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: "https://direct-subset-204118.firebaseio.com"
