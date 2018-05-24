b64b3e1eade3b40a3fc050217f8331449832ac60
vrinox <xonirv@gmail.com>
2018-05-24 12:59:41 +0200

    cambio

--- services/upload.js
+++ services/upload.js
@@ -1,6 +1,5 @@
 const upload = (file,usuario,firebase) => {
   let bucket = firebase.storage().bucket();
-  console.log(bucket.name);
   let prom = new Promise((resolve, reject) => {
     if (!file) {
       reject('No image file');
@@ -8,22 +7,22 @@ const upload = (file,usuario,firebase) => {
     let newFileName = `${usuario.id}_${Date.now()}`;
 
     let fileUpload = bucket.file(newFileName);
-    console.log("fileUpload:",fileUpload);
-    fileUpload.createWriteStream({
-      metadata: {
-        contentType: "image/jpeg"
-      }
+
+    fileUpload
+    .save(new Buffer(file.buffer))
+    .then((result) => {
+      console.log("resultado",result);
+      resolve({
+        status: 'success',
+        data: Object.assign({}, fileUpload.metadata, {
+          downloadURL: `https://storage.googleapis.com/${bucket.name}/${newFileName}`,
+        })
+      });
     })
-      .on('error', function(err) {
-        console.log("error en subida:",err);
-        reject('Something is wrong! Unable to upload at the moment.');
-      })
-      .on('finish', function() {
-        // The public URL can be used to directly access the file via HTTP.
-        const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
-        resolve(url);
-      })
-      .end(file.buffer);
+    .catch(err => {
+      console.log("error en subida:",err);
+      reject(err)
+    });
   });
   return prom;
 }
