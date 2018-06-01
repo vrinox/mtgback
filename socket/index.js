d5bad3ed402d408ff40474db4dc6fdcb1df4d7fe
vrinox <xonirv@gmail.com>
2018-06-02 00:44:53 +0200

    mejora policias

--- socket/index.js
+++ socket/index.js
@@ -5,14 +5,14 @@ const init = function(io){
     const UID = socket.handshake.query.usuario;
     let err,usuario;
     [err,usuario] = await to(Usuario.findOne({"where":{"id":UID}}));
-    if(err) next(new Error('authentication error'));
+    if(err) next(new Error('Socket: authentication error'));
     socket.usuario = usuario;
     next();
   });
   io.on('connection', (socket) => {
-    console.log('usuario conectado');
+    console.log('Socket: usuario conectado');
     socket.on('error',(err)=>{
-      console.log("socket error:",err);
+      console.log("Socket error:",err);
     });
     //auth en caso que no se pueda usar
     socket.on('auth',async (data)=>{
@@ -22,7 +22,7 @@ const init = function(io){
       }else{
           socket.usuario = usuario;
           socket.emit("auth",{success:true});
-          console.log("usuario auntenticado:",socket.usuario.username);
+          console.log("Socket: usuario "+socket.usuario.username+" auntenticado");
       }
     });
     require('./notificacion')(socket);
