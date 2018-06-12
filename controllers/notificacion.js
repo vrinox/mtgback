0e554d611ac1e9f68758f67b0d26fdb6e67dead9
vrinox <xonirv@gmail.com>
2018-06-12 15:58:56 +0200

    prueba 6

--- controllers/notificacion.js
+++ controllers/notificacion.js
@@ -11,8 +11,11 @@ const getAll = async function(req, res){
     }));
     if(err) ReE(res,{"success":false,"error":err});
     notificaciones = notificaciones.map(notificacion => {
-      console.log(notificacion.Invitacions);
+      console.log(notificacion);
+      let afitrion = notificacion.Invitacions[0].getUsuario();
+      console.log(anfitrion);
       let newNot = notificacion.toWeb();
+      newNot.anfitrion = anfitrion.toWeb();
       return newNot;
     });
     return ReS(res, {"success":true,"notificaciones":notificaciones});
