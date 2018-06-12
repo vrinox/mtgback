5cdfe959580d585e9b10786ce67515d353d14b47
vrinox <xonirv@gmail.com>
2018-06-12 15:31:10 +0200

    prueba 2

--- controllers/notificacion.js
+++ controllers/notificacion.js
@@ -3,31 +3,21 @@ const Invitacion    = require('../models').Invitacion;
 
 const getAll = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
-    let err, notificaciones,invitaciones
+    let err, notificaciones,
     usuario = req.user;
-
-    [err, invitaciones] = await to(Invitacion.findAll({
+    [err, notificaciones] = await to(Notificacion.findAll({
       include:[{
-        model:Notificacion
-      }],
+        model:Invitacion
+      }]
       where:{
         UsuarioId:usuario.id
       }
     }));
     if(err) ReE(res,{"success":false,"error":err});
-    invitaciones = invitaciones.map((invitacion)=>{
-      return invitacion.toWeb();
+    notificaciones = notificaciones.map(notificacion => {
+      let newNot = notificacion.toWeb();
+      return newNot;
     });
-    // [err, notificaciones] = await to(Notificacion.findAll({
-    //   where:{
-    //     UsuarioId:usuario.id
-    //   }
-    // }));
-    // notificaciones = notificaciones.map(notificacion => {
-    //   let newNot = notificacion.toWeb();
-    //   newNot.invitacion = notificacion.getInvitacions();
-    //   return newNot;
-    // });
     return ReS(res, {"success":true,"notificaciones":notificaciones});
 }
 module.exports.getAll = getAll;
