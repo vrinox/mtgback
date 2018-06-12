7763214c14f679145910f4fc33b97655f81cba41
vrinox <xonirv@gmail.com>
2018-06-12 15:48:29 +0200

    prueba 3

--- controllers/notificacion.js
+++ controllers/notificacion.js
@@ -6,14 +6,11 @@ const getAll = async function(req, res){
     let err, notificaciones,
     usuario = req.user;
     [err, notificaciones] = await to(Notificacion.findAll({
-      include:[{
-        model:Invitacion
-      }],
-      where:{
-        UsuarioId:usuario.id
-      }
+      include:[{model:Invitacion}],
+      where:{UsuarioId:usuario.id}
     }));
     if(err) ReE(res,{"success":false,"error":err});
+    console.log(notificaciones[1].Invitacions);
     notificaciones = notificaciones.map(notificacion => {
       let newNot = notificacion.toWeb();
       return newNot;
