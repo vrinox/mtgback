6221667f949f5e7efeb259a267c06f31101aae6c
vrinox <xonirv@gmail.com>
2018-10-18 14:08:04 +0200

    no se porq

--- routes/conectados.js
+++ routes/conectados.js
@@ -3,6 +3,7 @@ const Servidor = require('../socket').Servidor;
 const rutas = function(router){
   router.get(  '/conectados' ,  (req, res)=>{
 		let clientes = JSON.stringify(Servidor.clientes);
+		console.log(clientes);
 		res.statusCode = 200;//send the appropriate status code
 		res.json({status:"success", data:clientes});
 	});
