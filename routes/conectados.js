76c761a7d6032199601bf56bcd59807942e0110f
vrinox <xonirv@gmail.com>
2018-10-18 13:54:32 +0200

    esto es

--- routes/conectados.js
+++ routes/conectados.js
@@ -2,6 +2,6 @@ const Servidor = require('../socket').Servidor;
 const rutas = function(router){
   router.get(  '/conectados' ,  (req, res))=>{
 		res.statusCode = 200;//send the appropriate status code
-		res.json({status:"success", message:"MagicHub API", data:Servidor.clientes});
+		res.json({status:"success", data:Servidor.clientes});
 	});
 }
