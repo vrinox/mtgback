4232209e4bef508b2922fdb8f57b039a96e142e2
vrinox <xonirv@gmail.com>
2018-10-18 14:15:48 +0200

    prueba conectados

--- routes/conectados.js
+++ routes/conectados.js
@@ -2,9 +2,16 @@ const Servidor = require('../socket').Servidor;
 
 const rutas = function(router){
   router.get(  '/conectados' ,  (req, res)=>{
-		console.log(Servidor.clientes);
+		let clientes = Servidor.clientes.map((cliente)=>{
+			return {
+				Socket:{
+					id:cliente.id;
+				},
+				usuario: cliente.usuario.dataValues
+			};
+		})
 		res.statusCode = 200;//send the appropriate status code
-		res.json({status:"success", data:{}});
+		res.json({status:"success", data:clientes});
 	});
 }
 module.exports = rutas;
