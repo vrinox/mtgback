53ca11a42a96803babb67be2bb27a906055d1641
vrinox <xonirv@gmail.com>
2018-10-18 13:59:49 +0200

    error

--- routes/conectados.js
+++ routes/conectados.js
@@ -1,6 +1,7 @@
 const Servidor = require('../socket').Servidor;
+
 const rutas = function(router){
-  router.get(  '/conectados' ,  (req, res))=>{
+  router.get(  '/conectados' ,  (req, res)=>{
 		res.statusCode = 200;//send the appropriate status code
 		res.json({status:"success", data:Servidor.clientes});
 	});
