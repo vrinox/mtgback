2da79ab63438321bec1d1a9f078fb028f13a8353
vrinox <xonirv@gmail.com>
2018-10-18 13:53:00 +0200

    prueba mostrar conectados

---
+++ routes/conectados.js
@@ -0,0 +1,7 @@
+const Servidor = require('../socket').Servidor;
+const rutas = function(router){
+  router.get(  '/conectados' ,  (req, res))=>{
+		res.statusCode = 200;//send the appropriate status code
+		res.json({status:"success", message:"MagicHub API", data:Servidor.clientes});
+	});
+}
