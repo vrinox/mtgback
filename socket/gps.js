3f45391ec0fa1a86423a7fc36eb9563628c54639
vrinox <xonirv@gmail.com>
2018-11-10 18:40:16 +0100

    gps2

--- socket/gps.js
+++ socket/gps.js
@@ -1,14 +1,21 @@
 
 module.exports = async function(socket,server){
-    socket.on("gps",(data)=>{
-        console.log(data);
-        server
-            .getCliente(data.usuario)
-            .then((cliente)=>{
-                cliente.ultimaUbicacion = data.latlng;
-            })
-            .catch((err)=>{
-                console.log("GPS:",err);
-            })
-    });
+  socket.on("gps",(data)=>{
+    server
+      .getCliente(data.usuario)
+      .then((cliente)=>{
+        cliente.ultimaUbicacion = data.latlng;
+      })
+      .catch((err)=>{
+        console.log("GPS:",err);
+      })
+  });
+  socket.on("gps:disponibles",(usuarioId)=>{
+    console.log("GPS:disponibles "+usuarioId);
+    server
+      .getCliente(usuarioId)
+      .then((cliente)=>{
+        console.log("GPS:",cliente.ultimaUbicacion);
+      })
+  })
 }
