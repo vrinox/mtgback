cde9c1728e6aac8174ec5692686e34e6d4959fe8
vrinox <xonirv@gmail.com>
2018-11-10 19:49:12 +0100

    .-

--- socket/gps.js
+++ socket/gps.js
@@ -1,10 +1,12 @@
 
 module.exports = async function(socket,server){
   socket.on("gps",(data)=>{
+    console.log(data);
     server
       .getCliente(data.usuario)
       .then((cliente)=>{
         cliente.ubicacion = data.latlng;
+        console.log("llego",cliente.ubicacion);
       })
       .catch((err)=>{
         console.log("GPS:",err);
