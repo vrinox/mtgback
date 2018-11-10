36b415af23c65e96a05788b08a667ede4fb122cd
vrinox <xonirv@gmail.com>
2018-11-10 18:45:07 +0100

    gps bugfix recepcion de data

--- socket/gps.js
+++ socket/gps.js
@@ -10,10 +10,10 @@ module.exports = async function(socket,server){
         console.log("GPS:",err);
       })
   });
-  socket.on("gps:disponibles",(usuarioId)=>{
-    console.log("GPS:disponibles "+usuarioId);
+  socket.on("gps:disponibles",(data)=>{
+    console.log("GPS:disponibles "+data.usuarioId);
     server
-      .getCliente(usuarioId)
+      .getCliente(data.usuarioId)
       .then((cliente)=>{
         console.log("GPS:",cliente.ultimaUbicacion);
       })
