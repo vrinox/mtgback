a77e58c9d9d589441e139ca4f0ada3dd98fab89f
vrinox <xonirv@gmail.com>
2018-11-18 17:29:30 +0100

    rad

--- services/gpsHelper.js
+++ services/gpsHelper.js
@@ -5,10 +5,10 @@ const gpsHelper = {};
 
   gpsHelper.obtenerDistancia = function(cliente1, cliente2) {
     var R = 6378137; // Earth’s mean radius in meter
-    var dLat = rad(cliente2.ubicacion.lat - cliente1.ubicacion.lat);
-    var dLong = rad(cliente2.ubicacion.lng - cliente1.ubicacion.lng);
+    var dLat = this.rad(cliente2.ubicacion.lat - cliente1.ubicacion.lat);
+    var dLong = this.rad(cliente2.ubicacion.lng - cliente1.ubicacion.lng);
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(rad(cliente1.ubicacion.lat)) * Math.cos(rad(cliente2.ubicacion.lat)) *
+        Math.cos(this.rad(cliente1.ubicacion.lat)) * Math.cos(this.rad(cliente2.ubicacion.lat)) *
         Math.sin(dLong / 2) * Math.sin(dLong / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
