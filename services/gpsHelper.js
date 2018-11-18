2e1e74e70b5ab4807ab0473cba720cbd5bd5d1ae
vrinox <xonirv@gmail.com>
2018-11-18 17:24:23 +0100

    funciones del helper

--- services/gpsHelper.js
+++ services/gpsHelper.js
@@ -3,12 +3,12 @@ const gpsHelper = {};
     return x * Math.PI / 180;
   }
 
-  gpsHelper.obtenerDistancia = function(p1, p2) {
+  gpsHelper.obtenerDistancia = function(cliente1, cliente2) {
     var R = 6378137; // Earth’s mean radius in meter
-    var dLat = rad(p2.lat() - p1.lat());
-    var dLong = rad(p2.lng() - p1.lng());
+    var dLat = rad(cliente2.ubicacion.lat - cliente1.ubicacion.lat);
+    var dLong = rad(cliente2.ubicacion.lng - cliente1.ubicacion.lng);
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+        Math.cos(rad(cliente1.ubicacion.lat)) * Math.cos(rad(cliente2.ubicacion.lat)) *
         Math.sin(dLong / 2) * Math.sin(dLong / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
