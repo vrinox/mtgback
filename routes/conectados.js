62be2698157a3d57b5a2a7b43eab0ae3e018949a
Victor Manuel Leon <vleon@solofisa.sf>
2019-09-23 10:55:40 +0200

    nuevo

--- routes/conectados.js
+++ routes/conectados.js
@@ -2,7 +2,6 @@ const Servidor = require('../socket/servidor');
 
 const rutas = function(router){
   router.get(  '/conectados' ,  (req, res)=>{
-	console.log("Servidor:",Servidor);
     console.log(Servidor.clientes.map((cliente)=>{
 			return cliente.ubicacion;
 		}));
