36d5ed2571e985856dcb674a8c282ec3d829166c
Victor Manuel Leon <vleon@solofisa.sf>
2019-09-27 14:32:26 +0200

    nuevo

--- routes/conectados.js
+++ routes/conectados.js
@@ -2,20 +2,19 @@ const Servidor = require('../socket/servidor');
 
 const rutas = function(router){
   router.get(  '/conectados' ,  (req, res)=>{
-    console.log(Servidor.clientes.map((cliente)=>{
-			return cliente.ubicacion;
-		}));
-		let clientes = Servidor.clientes.map((cliente)=>{
-			return {
-					ubicacion	:	cliente.ubicacion,
-					id			:	cliente.usuario.dataValues.id,
-					nombre		:	cliente.usuario.dataValues.username,
-					socket		:	cliente.socket.id,
-					deviceId	:	cliente.usuario.dataValues.deviceId,
-					intervalId	:	cliente.intervalId,
-					idIntervalOponente	:	cliente.idIntervalOponente,
-			};
-		})
+	let clientes = Servidor.clientes.map((cliente)=>{
+		return {
+				ubicacion	:	cliente.ubicacion,
+				id			:	cliente.usuario.dataValues.id,
+				nombre		:	cliente.usuario.dataValues.username,
+				socket		:	cliente.socket.id,
+				deviceId	:	cliente.usuario.dataValues.deviceId,
+				intervalId	:	cliente.intervalId,
+				idIntervalOponente	:	cliente.idIntervalOponente,
+		};
+	})
+    console.log(clientes);
+		
 		res.statusCode = 200;//send the appropriate status code
 		res.json({usuarios:clientes.length, clientes:clientes});
 	});
