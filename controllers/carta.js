4a0c058083a75c79d7f6f44947c04bdf80efc2f4
victor leon <vleon@solofisa.sf>
2019-11-13 15:41:26 +0100

    nuevo

--- controllers/carta.js
+++ controllers/carta.js
@@ -41,7 +41,7 @@ const getAllCards = async function(req, res){
   let todas = [];
   mtg.card.all(filtrosAll)
   .on("data",(data)=>{
-    console.log("[CartasAll]:data",data.nombre);
+    console.log("[CartasAll]:data",data);
     todas.push(data);
   })
   mtg.card.all(filtrosAll)
