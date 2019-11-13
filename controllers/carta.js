89fdc0271461c2f7345077f6ef65cc9e37d76024
victor leon <vleon@solofisa.sf>
2019-11-13 14:05:34 +0100

    nuevo

--- controllers/carta.js
+++ controllers/carta.js
@@ -21,6 +21,27 @@ const getAll = async function(req, res){
 
 module.exports.getAll = getAll;
 
+const getAllCards = async function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  let err, cartas, filtros;
+  filtros = req.body.filtros;
+  //ordeno por orden alfabetico en caso de que no traiga un orden
+  if(!filtros.hasOwnProperty("orderBy")){
+    filtros.orderBy="name";
+  }
+  /*
+    para paginacion usa:
+      page    : pagina actual,
+      pageSize: registros por pagina
+  */
+ console.log("[Cartas]",filtros);
+  [err, cartas] = await to(mtg.card.all(filtros));
+  if(err) ReE(res, {success:false, error:err}, 422);
+  return ReS(res, {"cartas":cartas});
+}
+
+module.exports.getAllCards = getAllCards;
+
 const get = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
     let idCarta,carta;
